a79007bf2747d22f706ac1cba375f627775291cf
hadrihl <hadrihl@outlook.com>
2024-06-20 17:05:08 +0800

    Added UserService unit test.

--- src/test/java/com/example/spring_security_jsp_user/CommonControllerTest.java
+++ src/test/java/com/example/spring_security_jsp_user/CommonControllerTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @WebMvcTest(CommonController.class)
-public class ControllerTest {
+public class CommonControllerTest {
 	
 	@Autowired
     private MockMvc mockMvc;
